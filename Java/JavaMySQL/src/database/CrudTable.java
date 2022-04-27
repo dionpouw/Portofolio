d4f8e573aaaaa27add999974d0e921a4936da6ed
dionpouw <aldion.sumampouw@gmail.com>
2022-04-28 00:01:35 +0800

    Remove unused methods

--- Java/JavaMySQL/src/database/CrudTable.java
+++ Java/JavaMySQL/src/database/CrudTable.java
@@ -7,17 +7,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface CrudTable {
-    void createDatabase(Connection connection) throws SQLException;
-
-    void useDatabase(Connection connection) throws SQLException;
-
     void createTable(Connection connection) throws SQLException;
 
     void insertData(DailyDevotion dailyDevotion) throws SQLException;
 
     ResultSet getData(Connection connection) throws SQLException;
 
-    void updateData(DailyDevotion dailyDevotion) throws SQLException;
+    void updateData(DailyDevotion dailyDevotion, int id) throws SQLException;
 
     void deleteData(int id) throws SQLException;
 }
