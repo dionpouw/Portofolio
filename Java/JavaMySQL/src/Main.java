751e953a4aff7f6ed876b90fba447feb43ccfdb5
dionpouw <aldion.sumampouw@gmail.com>
2022-04-27 23:29:45 +0800

    Making menu, and switches for CRUD

--- Java/JavaMySQL/src/Main.java
+++ Java/JavaMySQL/src/Main.java
@@ -1,2 +1,55 @@
+import database.Database;
+import models.DailyDevotion;
+import utils.Utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.ResultSet;
+
 public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Database database = new Database();
+
+        while (true) {
+            try {
+                database.createTable(database.getConnection());
+                Utils.mainMenu();
+                int choices = Integer.parseInt(br.readLine());
+                switch (choices) {
+                    case 1: {
+                        System.out.print("Enter title : ");
+                        String title = br.readLine();
+                        System.out.print("Enter content : ");
+                        String content = br.readLine();
+                        DailyDevotion dailyDevotion = new DailyDevotion(title, content);
+                        database.insertData(dailyDevotion);
+                        break;
+                    }
+                    case 2: {
+                        ResultSet rs = database.getData(database.getConnection());
+                        while (rs.next()) {
+                            System.out.println("Id: " + rs.getInt("id"));
+                            System.out.println("Title: " + rs.getString("title"));
+                            System.out.println("Content: " + rs.getString("content"));
+                        }
+                        break;
+                    }
+                    case 3: {
+                        break;
+                    }
+                    case 4: {
+                        break;
+                    }
+                    case 5: {
+                        System.exit(0);
+                        break;
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
