3463500d9bdab9a57c310cd49448f3b30adf7e1e
dionpouw <aldion.sumampouw@gmail.com>
2022-04-28 00:02:43 +0800

    Remove unused setter method and set final to property's access modifier.

--- Java/JavaMySQL/src/models/DailyDevotion.java
+++ Java/JavaMySQL/src/models/DailyDevotion.java
@@ -2,35 +2,19 @@ package models;
 
 public class DailyDevotion {
     private int id;
-    private String title;
-    private String content;
+    private final String title;
+    private final String content;
 
     public DailyDevotion(String title, String content) {
         this.title = title;
         this.content = content;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 }
