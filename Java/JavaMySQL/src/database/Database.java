fa832ca471f564bd0e5ecb16b0052944f7e967be
dionpouw <aldion.sumampouw@gmail.com>
2022-04-28 00:02:58 +0800

    Fixing bugs, and remove unused method.

--- Java/JavaMySQL/src/database/Database.java
+++ Java/JavaMySQL/src/database/Database.java
@@ -2,11 +2,7 @@ package database;
 
 import models.DailyDevotion;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class Database implements CrudTable {
     // Make connection to database
@@ -14,19 +10,6 @@ public class Database implements CrudTable {
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/dailyDevotion", "root", "root");
     }
 
-    // Create database if not exists
-    public void createDatabase(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.executeUpdate("CREATE DATABASE IF NOT EXISTS dailyDevotion");
-    }
-
-    @Override
-    public void useDatabase(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.executeUpdate("USE dailyDevotion");
-    }
-
-    // Create database if not exists
     public void createTable(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         statement.executeUpdate("CREATE TABLE IF NOT EXISTS dailyDevotion " + "(id INT NOT NULL AUTO_INCREMENT, title VARCHAR(255)," + "content VARCHAR(512),PRIMARY KEY (id))");
@@ -35,8 +18,7 @@ public class Database implements CrudTable {
     // Get data from database
     public ResultSet getData(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("Select * from dailyDevotion");
-        return rs;
+        return statement.executeQuery("Select * from dailyDevotion");
     }
 
     // Insert data to database
@@ -46,12 +28,11 @@ public class Database implements CrudTable {
         statement.executeUpdate("INSERT INTO dailyDevotion (title, content) VALUES ('" + dailyDevotion.getTitle() + "', '" + dailyDevotion.getContent() + "')");
     }
 
-
     // Update data to database
-    public void updateData(DailyDevotion dailyDevotion) throws SQLException {
+    public void updateData(DailyDevotion dailyDevotion, int id) throws SQLException {
         Connection connection = getConnection();
         Statement statement = connection.createStatement();
-        statement.executeUpdate("Update dailyDevotion set title = '" + dailyDevotion.getTitle() + "', content = '" + dailyDevotion.getContent() + "',  where id = '" + dailyDevotion.getId() + "'");
+        statement.executeUpdate("Update dailyDevotion set title = '" + dailyDevotion.getTitle() + "', content = '" + dailyDevotion.getContent() + "' where id = '" + id + "'");
     }
 
     // Delete data to database
