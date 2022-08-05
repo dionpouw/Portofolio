e5b05445d1b761b0f4167b91b892187b712bd40f
dionpouw <aldion.sumampouw@gmail.com>
2022-08-05 23:43:22 +0800

    add apikey + base url

--- Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/util/Constant.kt
+++ Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/util/Constant.kt
@@ -2,5 +2,6 @@ package com.jeflette.dogbreedapplication.util
 
 object Constant {
 
-    private val baseUrl = "https://api.thedogapi.com/v1/"
+    val baseUrl = "https://api.thedogapi.com/v1/"
+    val apiKey = "25ce91f7-75c9-4cc6-8414-de420280057f"
 }
