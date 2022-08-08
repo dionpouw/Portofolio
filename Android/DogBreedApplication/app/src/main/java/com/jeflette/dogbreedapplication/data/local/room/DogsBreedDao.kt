8f6da9bcd4fcd8ab8e41773c642d47aea769f497
dionpouw <aldion.sumampouw@gmail.com>
2022-08-08 23:08:13 +0800

    Dao finish

--- Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/data/local/room/DogsBreedDao.kt
+++ Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/data/local/room/DogsBreedDao.kt
@@ -1,7 +1,25 @@
 package com.jeflette.dogbreedapplication.data.local.room
 
-import androidx.room.Dao
+import androidx.room.*
+import com.jeflette.dogbreedapplication.data.local.entity.Breed
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DogsBreedDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertBreed(breeds: List<Breed>)
+
+    @Query("SELECT * FROM breed_table")
+    fun getAllBreeds(): Flow<List<Breed>>
+
+    @Query("SELECT * FROM breed_table WHERE isFavorite = 1")
+    suspend fun getBreedFavorite(): List<Breed>
+
+    @Delete
+    suspend fun deleteBreed(breed: Breed)
+
+    @Update
+    suspend fun updateBreed(breed: Breed)
+
 }
