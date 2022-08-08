60b353d1f283737391e83edb3e72a947fe9fcf03
dionpouw <aldion.sumampouw@gmail.com>
2022-08-08 23:10:22 +0800

    ViewModel for List Fragment & Search Feature

--- Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/ui/doglist/DogListViewModel.kt
+++ Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/ui/doglist/DogListViewModel.kt
@@ -1,7 +1,31 @@
 package com.jeflette.dogbreedapplication.ui.doglist
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.jeflette.dogbreedapplication.data.DogsRepository
+import com.jeflette.dogbreedapplication.data.remote.response.SearchResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DogListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class DogListViewModel @Inject constructor(
+    private val repository: DogsRepository
+) : ViewModel() {
+
+    val dogResponse = repository.getBreeds().asLiveData()
+
+    private val dogsLiveData = MutableLiveData<SearchResponse?>()
+    val breeds: LiveData<SearchResponse?> = dogsLiveData
+
+    private val isLoadingLiveData = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = isLoadingLiveData
+
+    fun getBreedSearch(query: String) {
+        viewModelScope.launch {
+            isLoadingLiveData.value = true
+            dogsLiveData.value = repository.searchBreed(query)
+            isLoadingLiveData.value = false
+        }
+    }
+
 }
