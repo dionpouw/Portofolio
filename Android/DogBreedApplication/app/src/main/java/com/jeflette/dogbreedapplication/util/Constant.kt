a1eb6308cbdfb829224a7a35d4262be7e477c1fa
dionpouw <aldion.sumampouw@gmail.com>
2022-08-14 01:46:41 +0800

    add const for baseUrl + apiKey

--- Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/util/Constant.kt
+++ Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/util/Constant.kt
@@ -2,6 +2,6 @@ package com.jeflette.dogbreedapplication.util
 
 object Constant {
 
-    val baseUrl = "https://api.thedogapi.com/v1/"
-    val apiKey = "25ce91f7-75c9-4cc6-8414-de420280057f"
+    const val baseUrl = "https://api.thedogapi.com/v1/"
+    const val apiKey = "25ce91f7-75c9-4cc6-8414-de420280057f"
 }
