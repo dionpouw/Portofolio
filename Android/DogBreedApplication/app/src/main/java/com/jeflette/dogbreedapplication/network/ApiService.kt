57fca4012293385cf9a7e99b05586fd0979b3626
dionpouw <aldion.sumampouw@gmail.com>
2022-08-14 01:47:22 +0800

    Fix Api service error due to expected array

--- Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/network/ApiService.kt
+++ Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/network/ApiService.kt
@@ -1,6 +1,6 @@
 package com.jeflette.dogbreedapplication.network
 
-import com.jeflette.dogbreedapplication.data.remote.response.BreedResponse
+import com.jeflette.dogbreedapplication.data.local.entity.Breed
 import com.jeflette.dogbreedapplication.data.remote.response.SearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,10 +8,10 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("breeds")
-    suspend fun getBreedList(): List<BreedResponse>
+    suspend fun getBreedList(): List<Breed>
 
     @GET("breeds/search")
     suspend fun getSearchBreed(
         @Query("q") query: String
-    ): SearchResponse
+    ): List<SearchResponse>
 }
