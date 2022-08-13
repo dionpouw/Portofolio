b9a39cfafa2e16db1f8193c073489f10e026cce8
dionpouw <aldion.sumampouw@gmail.com>
2022-08-14 01:50:18 +0800

    dog favorite viewModel

--- Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/ui/dogfavorite/DogFavoriteViewModel.kt
+++ Android/DogBreedApplication/app/src/main/java/com/jeflette/dogbreedapplication/ui/dogfavorite/DogFavoriteViewModel.kt
@@ -1,7 +1,25 @@
 package com.jeflette.dogbreedapplication.ui.dogfavorite
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jeflette.dogbreedapplication.data.DogsRepository
+import com.jeflette.dogbreedapplication.data.local.entity.Breed
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DogFavoriteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class DogFavoriteViewModel @Inject constructor(
+    private val repository: DogsRepository
+) : ViewModel() {
+
+    private var dogFavoriteList = MutableLiveData<List<Breed>>()
+    val dogFavorite get() = dogFavoriteList
+
+    fun getFavoriteDogs() {
+        viewModelScope.launch {
+            dogFavoriteList.value = repository.getBreedFavorite()
+        }
+    }
 }
